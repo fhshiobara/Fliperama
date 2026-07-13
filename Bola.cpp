53a52934c62773b3ad59fe89c0c8502329d7f6c4
fhshiobara <fhshiobara@gmail.com>
2026-07-12 21:14:46 -0300

    Inclusao da classe bola

---
+++ Bola.cpp
@@ -0,0 +1,8 @@
+//
+//  Bola.cpp
+//  Fliperama
+//
+//  Created by Edison Shiobara on 12/07/26.
+//
+
+#include "Bola.hpp"
---
+++ Bola.hpp
@@ -0,0 +1,13 @@
+//
+//  Bola.hpp
+//  Fliperama
+//
+//  Created by Edison Shiobara on 12/07/26.
+//
+
+#ifndef Bola_hpp
+#define Bola_hpp
+
+#include <stdio.h>
+
+#endif /* Bola_hpp */
