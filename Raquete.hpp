ab16278169bb5daab764645ded20ff840c2cfb96
fhshiobara <fhshiobara@gmail.com>
2026-07-13 12:30:43 -0300

    mudanca no cmake e adicao do raquete hpp/cpp

--- Raquete.hpp
+++ Raquete.hpp
@@ -2,35 +2,11 @@
 //  Raquete.hpp
 //  Fliperama
 //
-//  Created by Edison Shiobara on 12/07/26.
+//  Created by Edison Shiobara on 13/07/26.
 //
 
-#ifndef Raquete_hpp
-#define Raquete_hpp
-
-#include <stdio.h>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include "VectorTL.hpp"
 
-class Raquete{
-private:
-    CoordF pos;
-    CoordF tam;
-    CoordF velocidade;
-    sf::RectangleShape* sprite;
-public:
-    Raquete(CoordF p, CoordF t,CoordF v);
-    ~Raquete();
-    
-    
-    void setPos(CoordF p){pos = p;}
-    CoordF getPos(){return pos;}
-    void setTam(CoordF t){tam = t;}
-    CoordF getTam(){return tam;}
-    void setVel(CoordF v){velocidade = v;}
-    CoordF getVel(){return velocidade;}
-    
-    void moverY(float dt);
-};
 
-#endif /* Raquete_hpp */
