40a0c7d5c16bf52e5b51c83d0caf124e5a5225d1
fhshiobara <fhshiobara@gmail.com>
2026-07-18 21:23:28 -0300

    criacao de entidade

---
+++ Entidade.cpp
@@ -0,0 +1,8 @@
+//
+//  Entidade.cpp
+//  Fliperama
+//
+//  Created by Edison Shiobara on 18/07/26.
+//
+
+#include "Entidade.hpp"
---
+++ Entidade.hpp
@@ -0,0 +1,13 @@
+//
+//  Entidade.hpp
+//  Fliperama
+//
+//  Created by Edison Shiobara on 18/07/26.
+//
+
+#ifndef Entidade_hpp
+#define Entidade_hpp
+
+#include <stdio.h>
+
+#endif /* Entidade_hpp */
